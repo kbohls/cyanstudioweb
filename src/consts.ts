e41b8440a7c4dd744c0214818d9373f8e42d3049
Kurtis Bohlen <KurtisBohlen@gmail.com>
2026-04-18 22:29:26 -0700

    Search engine optimizations

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Cyan Studio";
-export const SITE_DESCRIPTION = "Original cyanotype prints handcrafted in Ucluelet, BC. Sun-printed using hand-collected seaweeds and coastal botanicals.";
+export const SITE_TITLE = "Cyan Studio | Cyanotype Prints from Ucluelet, BC";
+export const SITE_DESCRIPTION = "Original cyanotype prints handcrafted in Ucluelet, BC. Sun-printed using hand-collected seaweeds and coastal botanicals by artist Krista Bohlen.";
+export const SITE_URL = "https://cyanstudio.ca";
